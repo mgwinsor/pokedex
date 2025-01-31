03bbe1e2feff6a14949b8e859947f99958079a7b
Michael Winsor <mgwinsor@gmail.com>
2025-01-31 17:38:43 +0800

    add: repl

--- repl.go
+++ repl.go
@@ -1,6 +1,27 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+		fmt.Println("Your command was:", commandName)
+	}
+}
 
 func cleanInput(text string) []string {
 	lowerText := strings.ToLower(text)
